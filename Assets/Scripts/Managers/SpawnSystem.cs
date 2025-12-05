8fa5490eb8f5f7d2de4208730e31887457ae86a2
Mads Lundsby Bisgaard <mads@malbi.dk>
2025-12-05 11:06:30 +0100

    Implement bubble spawning system
    Updated SpawnSystem to periodically spawn bubbles at random positions within the camera's orthographic bounds using the bubble prefab.

--- Assets/Scripts/Managers/SpawnSystem.cs
+++ Assets/Scripts/Managers/SpawnSystem.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class SpawnSystem : MonoBehaviour
 {
+    public GameObject bubble;
+    Camera cam;
+
+    public void SpawnBubble()
+    {
+        float height = cam.orthographicSize;
+        float width = height * cam.aspect;
+
+        float spawnPointX = Random.Range(-width, width);
+        float spawnPointY = Random.Range(-height, height);
+
+        Debug.Log($"Screen Width in World Units: {width}");
+
+        Vector2 spawnPosition = new Vector2(spawnPointX, spawnPointY);
+
+        Instantiate(bubble, spawnPosition, Quaternion.identity);
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        cam = Camera.main;
+        InvokeRepeating(nameof(SpawnBubble), 0.1f, 0.1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
