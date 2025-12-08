43b537e1964e44ef95e3ced9934f14b43e0f89a2
Mads Lundsby Bisgaard <mads@malbi.dk>
2025-12-08 11:31:49 +0100

    Rename Test.cs to ObjectPoolTest.cs and clean up
    Renamed Test.cs to ObjectPoolTest.cs for clarity and updated the class name accordingly. Removed unused Test.cs and its meta file from the Assets directory.

--- Assets/Scripts/Gameplay Systems/Object pooler/ObjectPoolTest.cs
+++ Assets/Scripts/Gameplay Systems/Object pooler/ObjectPoolTest.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Test : Poolable
+public class ObjectPoolTest : Poolable
 {
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
