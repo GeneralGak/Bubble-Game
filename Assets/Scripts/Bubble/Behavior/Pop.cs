c76344c2dfaed40adb07e2abf671416f07127a0b
Hvemslev <111503814+Hvemslev@users.noreply.github.com>
2025-12-15 17:39:33 +0100

    bubble pop sfx
    added popping sound to bubbles (sometimes the audio bugs out a little. idk how to fix)

--- Assets/Scripts/Bubble/Behavior/Pop.cs
+++ Assets/Scripts/Bubble/Behavior/Pop.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Pop : BubbleBehavior
 {
+    public AudioSource popSfx;
+    
     public override void RunOnPointerDown()
 	{
         StartCoroutine(popDelay());
         GetComponent<Animation>().Play("bubbleBurst");
+        popSfx.Play();
     }
 
     //IEnumerator required for delay between animation start and removal of object
 	IEnumerator popDelay()
     {
-        yield return new WaitForSeconds(0.13f);
+        yield return new WaitForSeconds(0.135f);
         PoolableComponent.RemoveToPool();
     }
 }
