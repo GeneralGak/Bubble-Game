a6481cd7e39a157050f8f796dcd51577d4c3a8a4
Hvemslev <111503814+Hvemslev@users.noreply.github.com>
2025-12-15 16:55:07 +0100

    bubble burst animation
    added burst animation and delay before removal to bubbles

--- Assets/Scripts/Bubble/Behavior/Pop.cs
+++ Assets/Scripts/Bubble/Behavior/Pop.cs
@@ -1,10 +1,19 @@
+using System.Collections;
 using UnityEngine;
 
 
 public class Pop : BubbleBehavior
 {
-	public override void RunOnPointerDown()
+    public override void RunOnPointerDown()
 	{
-		PoolableComponent.RemoveToPool();
-	}
+        StartCoroutine(popDelay());
+        GetComponent<Animation>().Play("bubbleBurst");
+    }
+
+    //IEnumerator required for delay between animation start and removal of object
+	IEnumerator popDelay()
+    {
+        yield return new WaitForSeconds(0.13f);
+        PoolableComponent.RemoveToPool();
+    }
 }
