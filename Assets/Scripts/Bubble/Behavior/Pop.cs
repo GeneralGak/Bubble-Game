6b58d0069bc63e7b2aafea24b82cd01a678fc8db
Hvemslev <111503814+Hvemslev@users.noreply.github.com>
2025-12-16 14:11:26 +0100

    pop variations
    added three random pop sfx

--- Assets/Scripts/Bubble/Behavior/Pop.cs
+++ Assets/Scripts/Bubble/Behavior/Pop.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Pop : BubbleBehavior
 {
-    public AudioSource popSfx;
+    public AudioSource popSfx1;
+    public AudioSource popSfx2;
+    public AudioSource popSfx3;
+
+    int sfxPicker;
     
     public override void RunOnPointerDown()
 	{
         StartCoroutine(popDelay());
         GetComponent<Animation>().Play("bubbleBurst");
-        popSfx.Play();
+
+        sfxPicker = Random.Range(0, 3);
+        
+        if(sfxPicker == 0)
+        {
+            popSfx1.Play();
+        }
+        else if(sfxPicker == 1)
+        {
+            popSfx2.Play();
+        }
+        else
+        {
+            popSfx3.Play();
+        }
     }
 
     //IEnumerator required for delay between animation start and removal of object
